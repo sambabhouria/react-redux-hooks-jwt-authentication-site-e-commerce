2d7a9548a2c6dd20b13921d2b25dcc52d517210f
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-03-08 22:55:56 +0100

    icon awsome add

--- src/client/signup/signup.js
+++ src/client/signup/signup.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./singup.css";
+
 class Signup extends Component {
   state = {
     username: "",
