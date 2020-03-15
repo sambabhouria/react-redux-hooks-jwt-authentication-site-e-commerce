4d3663ec82f6f1d5e13dd4d9169aa39b0714a14a
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-03-15 15:57:44 +0100

    local product added for pagination

--- src/client/pagination/product.js
+++ src/client/pagination/product.js
@@ -1,9 +1,4 @@
-/*
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */
-
-export const storeProducts = [
+export const LocalProducts = [
   {
     id: 1,
     title: "Google Pixel - Black",
@@ -678,16 +673,3 @@ export const storeProducts = [
     total: 0
   }
 ];
-
-export const detailProduct = {
-  id: 1,
-  title: "Google Pixel - Black",
-  img: "img/product-1.png",
-  price: 10,
-  company: "google",
-  info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-  inCart: false,
-  count: 0,
-  total: 0
-};
